e2e6ef54334156fda52030a0813a71af2585953d
shompaaa <shompaakter334@gmail.com>
2026-04-06 17:35:48 +0600

    password validation done

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,12 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../../components/AuthProvider/AuthProvider";
 
 const Register = () => {
+  const [error, setError] = useState("");
+  const { registerUser } = useContext(AuthContext);
 
-    const {registerUser} = useContext(AuthContext)
+  const specialCharacter = /[!@#$%^&*(),.?":{}|<>]/;
+
   const handleRegister = (e) => {
     e.preventDefault();
     const name = e.target.name.value;
@@ -11,34 +14,83 @@ const Register = () => {
     const email = e.target.email.value;
     const password = e.target.password.value;
     const confirmPassword = e.target.confirmPassword.value;
-    console.log(name, photo,email,password,confirmPassword);
-    registerUser(email,password)
+    console.log(name, photo, email, password, confirmPassword);
+    registerUser(email, password);
+
+
+    //Error Reset
+    setError('')
+
+    if(password.length < 6){
+        setError('password must be at least 6 characters')
+        return
+    }
+    
+    else if(password !== confirmPassword){
+        setError("password didn't match")
+        return
+    }
+    else if(!specialCharacter.test(password)){
+        setError('must include special characters (e.g: @,#,$ etc)')
+        return
+    }
+
   };
   return (
     <div className="w-[20%] mx-auto min-w-125 border-2 border-red-400 p-2 rounded-xl">
       <form onSubmit={handleRegister}>
         <div>
           <p>Name</p>
-          <input type="text" name="name" placeholder="Name here" className="input w-full" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Name here"
+            className="input w-full"
+          />
         </div>
         <div>
           <p>Photo</p>
-          <input type="text" name="photo" placeholder="Photo" className="input w-full" />
+          <input
+            type="text"
+            name="photo"
+            placeholder="Photo"
+            className="input w-full"
+          />
         </div>
         <div>
           <p>Email</p>
-          <input type="email" name="email" placeholder="Email here" className="input w-full" />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email here"
+            className="input w-full"
+          />
         </div>
         <div>
           <p>Password</p>
-          <input type="password" name="password" placeholder="Password here" className="input w-full" />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password here"
+            className="input w-full"
+          />
         </div>
         <div className="mb-2">
           <p>Confirm Password</p>
-          <input type="password" name="confirmPassword" placeholder="Confirm password here" className="input w-full" />
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm password here"
+            className="input w-full"
+          />
         </div>
+        {
+            error && <small className="text-red-500">{error}</small>
+        }
         <div>
-          <button type="submit" className="btn btn-primary w-full">Register</button>
+          <button type="submit" className="btn btn-primary w-full">
+            Register
+          </button>
         </div>
       </form>
     </div>
